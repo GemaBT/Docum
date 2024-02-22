1dad975803498c0cdee742b476cfa60d1fbbb126
GBT <gemablancodetoro@gmail.com>
2024-02-22 21:14:55 +0100

    Comentarios Javadoc eliminados

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,90 +1,37 @@
-/**
- * Clase Empleado
- *
- * Contiene informacion de cada empleado
- *
- * @author Gema
- * @version 1.0
- */
 public class Empleado{
 
-    /**
-     * Constante SALARIO_DEF
-     */
     public final static double SALARIO_DEF=600;
 
-    /**
-     * Indica que la edad de un empleado es mayor que otro
-     */
     public final static int MAYOR=1;
 
-    /**
-     * Indica que las edades de los empleados son iguales
-     */
     public final static int IGUAL=0;
 
-    /**
-     * Indica que la edad de un empleado es menor que otro
-     */
     public final static int MENOR=-1;
 
-    /**
-     * Nombre del empleado
-     */
     private String nombre;
 
-    /**
-     * Apellido del empleado
-     */
     private String apellido;
     
-    /**
-     * Edad del empleado
-     */
     private int edad;
     
-    /**
-     * Salario del empleado
-     */
     private double salario;
     
-    /**
-     * Devuelve el nombre del empleado
-     * @return nombre del empleado
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * Devuelve el apellido del empleado
-     * @return apellido del empleado
-     */
     public String getApellido() {
         return apellido;
     }
 
-    /**
-     * Devuelve la edad de un empleado
-     * @return edad del empleado
-     */
     public int getEdad() {
         return edad;
     }
 
-    /**
-     * Devuelve el salarioBase
-     * @return salarioBse
-     */
     public double getSalario() {
         return salario;
     }
 
-    /**
-     * Suma un plus si el empleado tiene mas de 40 años
-     * @param sueldoPlus
-     * @return true si se realiza y false si no se realiza
-     */
     public boolean plus (double sueldoPlus){
 
         boolean aumento=false;
@@ -95,11 +42,6 @@ public class Empleado{
         return aumento;
     }
 
-    /**
-     * Indica si dos empleados son iguales segun su nombre y apellido
-     * @param a empleado a comparar
-     * @return true si son iguales y false si no lo son
-     */
     public boolean equals (Empleado a){
 
         if(a.getNombre().equals(nombre) && a.getApellido().equals(apellido)){
@@ -109,11 +51,6 @@ public class Empleado{
         }
     }
 
-    /**
-     * Indica si un empleado es mayor, igual o menor que otro segun la edad
-     * @param a empleado a comparar
-     * @return 1: mayor, 0: iguales y -1 menor que el empleado comparado
-     */
     public int compareTo(Empleado a){
 
         int estado=MENOR;
@@ -126,10 +63,6 @@ public class Empleado{
 
     }
 
-    /**
-     * Devuelve el estado del empleado
-     * @return estado del empleado
-     */
     public String toString (){
         String mensaje="El empleado se llama "+nombre+" "+apellido+" con "+edad+" años " +
                 "y un salario de "+salario;
@@ -137,39 +70,18 @@ public class Empleado{
     }
 
 
-    /**
-     * Constructor por defecto
-     */
     public Empleado(){
         this ("", "", 0, SALARIO_DEF);
     }
 
-    /**
-     * Constructor 
-     * @param nombre nombre del empleado
-     * @param apellido apellido del empleado
-     */
     public Empleado(String nombre, String apellido){
         this (nombre, apellido, 0, SALARIO_DEF);
     }
 
-    /**
-     * Constructor 
-     * @param nombre nombre del empleado
-     * @param apellido apellido del empleado
-     * @param edad edad del empleado
-     */
     public Empleado (String nombre, String apellido, int edad){
         this (nombre, apellido, edad, SALARIO_DEF);
     }
 
-    /**
-     * Constructor con 4 parametros
-     * @param nombre nombre del empleado
-     * @param apellido apellido del empleado
-     * @param edad edad del empleado
-     * @param salario salario del empleado
-     */
     public Empleado(String nombre, String apellido, int edad, double salario){
         this.nombre=nombre;
         this.apellido=apellido;
